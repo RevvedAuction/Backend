cc467304e31a02f3f88a493183de7e290e3fc547
Fhumu <113695945+KodeKnack@users.noreply.github.com>
2025-08-26 22:22:10 +0200

    payment back end technically done, this is a partial commit to avoid conflicting with the version control as we have different progresses on our local compared to our version

--- src/main/java/za/co/revvedAuctions/repository/IPaymentRepository.java
+++ src/main/java/za/co/revvedAuctions/repository/IPaymentRepository.java
@@ -1,9 +1,10 @@
 package za.co.revvedAuctions.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import za.co.revvedAuctions.domain.Payment;
 import java.util.List;
 
-public interface IPaymentRepository extends IRepository<Payment, Integer> {
+public interface IPaymentRepository extends JpaRepository<Payment, Integer> {
     List<Payment> findByBuyerID(int buyerId);
     List<Payment> findByAuctionID(int auctionId);
     List<Payment> findByStatus(String status);
